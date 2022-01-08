408811449ff4fc17bda5a32b422538021237d48f
himal999 <himal.official99@gmail.com>
2022-01-08 16:47:23 +0530

    Project Init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const userRoutes = require('./routes/user');
+//app
+const app = express();
+
+//routes
+app.use(userRoutes);
+
+//db
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+  
+}).then(()=>console.log('DB Connected'));
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port,()=>{
+    console.log(`Server is Running on ${port}`);
+});
